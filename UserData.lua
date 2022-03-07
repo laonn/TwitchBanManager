53ea7190eb4de10e4ac7ad8f6e4e0d38354e446d
Laon <grmedia@naver.com>
2022-03-07 22:35:44 +0900

    초기 커밋.

--- UserData.lua
+++ UserData.lua
@@ -1,23 +1,4 @@
 UserData =
 {
-	[1] = {
-		['Name'] = 'Tare',
-		['Date'] = 7178
-	},
-	[2] = {
-		['Name'] = 'Llaon',
-		['Date'] = 530
-	},
-	[3] = {
-		['Name'] = 'hacksun',
-		['Date'] = 2001
-	},
-	[4] = {
-		['Name'] = 'maple',
-		['Date'] = 19
-	},
-	[5] = {
-		['Name'] = 'lua',
-		['Date'] = 1155
-	}
+	
 }
